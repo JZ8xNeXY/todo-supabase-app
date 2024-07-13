96ad81c7f55bd1b318f443c1b9ed1e557ee1a4a7
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-13 20:43:48 +0900

    Layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,6 +5,7 @@ type Title = {
   title: string
   children: ReactNode
 }
+
 export const Layout: FC<Title> = ({ children, title = 'Todo app' }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-mono text-gray-800">
